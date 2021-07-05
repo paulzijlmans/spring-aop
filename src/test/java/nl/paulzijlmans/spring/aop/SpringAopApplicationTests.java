7a8b964c8113a929b67277694f230fb31781ce3d
Paul Zijlmans <paulzijlmans@hotmail.com>
2021-07-05 16:22:25 +0200

    first commit

---
+++ src/test/java/nl/paulzijlmans/spring/aop/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.paulzijlmans.spring.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
